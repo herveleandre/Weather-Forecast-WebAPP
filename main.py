8325652923233cae66c771a731f3022662bf6d5c
herveleandre <47299858+herveleandre@users.noreply.github.com>
2023-02-04 14:47:05 -0500

    added sky data, fixed errors. Complete

--- main.py
+++ main.py
@@ -10,12 +10,26 @@ days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the numb
 option = st.selectbox("Select Data Below", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+try:
+    if place:
 
+        filter_data = get_data(place, days)
 
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"] / 10 for dict in filter_data]
+            dates = [dict["dt_txt"] for dict in filter_data]
 
+            figure = px.line(x=dates, y=temperature,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-d, t = get_data(days)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
 
-figure = px.line(x=d, y=t,
-                 labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+            sky_condition = [dict["weather"][0]["main"] for dict in filter_data]
+            image_path = [images[condition] for condition in sky_condition]
+            print(sky_condition)
+            st.image(image_path, width=80)
+except KeyError:
+    (st.subheader(f"{place} is not on the list"))
